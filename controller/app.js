cad42feaa0853fb096d468824931a209dbe49c98
EnzoRafaell <enzo.aguiar@escola.pr.gov.br>
2026-06-22 08:30:18 -0300

    Update app.js

--- controller/app.js
+++ controller/app.js
@@ -329,3 +329,40 @@ window.encerrarConfrontos = function (id) {
         renderizarTudo();
     }
 };
+
+
+
+
+//// BASE_URL aponta para o JSON local enquanto a API não está integrada.
+// Quando a API estiver pronta, basta trocar para: 'http://localhost:3000/'
+//const BASE_URL = 'http://localhost:3000/api/';
+
+
+//async function _get(endpoint) {
+//    try{
+//        const response = await fetch (`${BASE_URL}${endpoint}`);
+//        if (!response.ok){
+//            throw new Error(`Erro ao buscar os ${endpoint}: ${response.statusText}`);
+//        }
+    
+//    const data = await response.json();
+//    return data;
+//    }
+//    catch (error) {
+//        console.error(`Erro ao buscar ${endpoint}:`, error);
+//        return [];
+//    }
+//}
+
+//// Retorna todos os times
+//async function getTimes() {
+//return _get 
+}
+// Retorna todos os competidores
+async function getCompetidores() {
+return _get
+}
+// Retorna todos os confrontos
+async function getConfrontos() {
+return _get
+}//
